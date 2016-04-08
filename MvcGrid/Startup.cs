0c3fcdf573c75214e8042bde9a724d751f223d6d
Moinul Islam <moinulmithu@hotmail.com>
2016-04-08 18:13:55 +0600

    Paged List Project added

---
+++ MvcGrid/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcGrid.Startup))]
+namespace MvcGrid
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
